2d8e31cbbef7d0b79323d5af0aff45ed8d295f0a
belbeche <wbelbeche.s@gmail.com>
2023-07-08 03:27:47 +0200

    small edit

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,7 +22,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'En vous inscrivant, vous acceptez nos conditions d\'utilisation du produit.',
                     ]),
                 ],
             ])
@@ -33,11 +33,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrez un mot de passe !',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe ne doit pas dépasser {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
