e848795d0f6886d4745826241a6b9788c223ee79
belbeche <wbelbeche.s@gmail.com>
2023-07-16 14:29:48 +0200

    edited email template controller

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -77,12 +77,12 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $exception->getReason());
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_login');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre adresse e-mail a été vérifiée.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
